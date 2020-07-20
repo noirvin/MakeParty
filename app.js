feb6133b3a71693782728e4a7d4d293e520e2c19
noirvin <vin.sp97@googlemail.com>
2020-07-20 05:15:40 -0400

    start script

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 const models = require('./db/models');
+require('./controllers/events')(app, models);
 
 
 app.use(methodOverride('_method'))
@@ -27,12 +28,6 @@ var events = [
   { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
 ]
 
-// Index
-app.get('/', (req, res) => {
-  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
-    res.render('events-index', { events: events });
-  })
-})
 
 
 app.get('/events/new', (req, res) => {
