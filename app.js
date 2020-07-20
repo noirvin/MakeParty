81984ec19d22d037abab467983f6621784993ae0
noirvin <vin.sp97@googlemail.com>
2020-07-20 04:24:42 -0400

    forgot to commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Initialize express
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+const models = require('./db/models');
 
 // require handlebars
 var exphbs = require('express-handlebars');
@@ -21,11 +24,26 @@ var events = [
   { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
 ]
 
-// INDEX
+// Index
 app.get('/', (req, res) => {
-  res.render('events-index', { events: events });
+  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+    res.render('events-index', { events: events });
+  })
 })
 
+
+app.get('/events/new', (req, res) => {
+  res.render('events-new', {});
+})
+
+// CREATE
+app.post('/events', (req, res) => {
+  models.Event.create(req.body).then(event => {
+    res.redirect(`/`);
+  }).catch((err) => {
+    console.log(err)
+  });
+})
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
 
