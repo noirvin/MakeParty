2816b84f9c2cf81de90dc75adc042523d664d693
noirvin <vin.sp97@googlemail.com>
2020-07-20 05:35:32 -0400

    delete rsvp

--- controllers/rsvps.js
+++ controllers/rsvps.js
@@ -24,5 +24,14 @@ module.exports = (app, models) => {
         console.log(err.message);
     })
 });
-  // DESTROY
+ 
+  // DELETE
+  app.delete('/events/:eventId/rsvps/:id', (req, res) => {
+      models.Rsvp.findByPk(req.params.id).then(rsvp => {
+          rsvp.destroy();
+          res.redirect(`/events/${req.params.eventId}`);
+      }).catch((err) => {
+          console.log(err);
+      });
+  });
 }
